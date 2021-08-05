2450d0ea09fdf3321e7de21a9e6d1c411b69e7bb
Shahed Chy Suzan <shahedchysuzan@gmail.com>
2021-08-05 11:24:36 +0600

    changed maxattempt+decayMinutes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -28,6 +28,9 @@ class LoginController extends Controller
      */
     protected $redirectTo = RouteServiceProvider::HOME;
 
+    protected $maxAttempts = 3; // Default is 5
+    protected $decayMinutes = 5; // Default is 1
+
     /**
      * Create a new controller instance.
      *
